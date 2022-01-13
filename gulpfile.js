c5a68b8639750e946ccef4a763087439c533c236
GuiLeoni14 <guileone167@gmail.com>
2022-01-13 15:18:25 -0200

    aprendendo sobre gulp-autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,17 @@
 const gulp = require('gulp');
 const sass =  require('gulp-sass')(require('sass'));
+const autoprefixer = require('gulp-autoprefixer');
 
 function compileSass() {
     return gulp.src('./src/scss/*.scss') // o ' * ' pega todos os arquivos com a extensão scss
     .pipe(sass({
         outputStyle: 'compressed',
     }))
-    .pipe(gulp.dest('./src/css'));
+    .pipe(autoprefixer({
+        overrideBrowserslist: ['last 2 versions'],
+        cascade: false,
+    }))
+    .pipe(gulp.dest('./src/css/'));
 }
 
-gulp.task('sass', compileSass);
+gulp.task('default', compileSass); // gulp.task('sass', compileSass);
