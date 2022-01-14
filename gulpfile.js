a503b1365c60001281a3bcba71b306eb8b48601e
GuiLeoni14 <guileone167@gmail.com>
2022-01-14 10:12:42 -0200

    integrando com bibliotecas externas

--- gulpfile.js
+++ gulpfile.js
@@ -46,11 +46,33 @@ function gulpJs(){
 gulp.task('allJs', gulpJs);
 gulp.task('browser-sync', browser);
 
+function pluginsCSS() {
+    return gulp.src('./src/css/lib/*.css')
+    .pipe(concat('plugins.css'))
+    .pipe(gulp.dest('./src/css/'))
+    .pipe(browserSync.stream())
+}
+
+gulp.task('plugincss', pluginsCSS);
+
+function pluginsJs() {
+    return gulp
+    .src(['./src/js/lib/aos.min.js','./src/js/lib/swiper.min.js'])
+    .pipe(concat('plugins.js'))
+    .pipe(gulp.dest('./src/js/'))
+    .pipe(browserSync.stream())
+}
+
+gulp.task('pluginjs', pluginsJs);
+
 // espera uma atualização para realizar um tarefa sem precisar reiniciar o gulp
 function watch() {
     gulp.watch('./src/scss/*.scss', compileSass); // gulp.series'ou parallel'('sass', 'outra-tarefa');
+    gulp.watch('./src/css/lib/*.css', pluginsCSS);
     gulp.watch('*.html').on('change', browserSync.reload); // browserSync.reload atualiza á pagina inteira
     gulp.watch('./src/js/scripts/*.js', gulpJs);
+    gulp.watch('./src/js/lib/*.js', pluginsJs);
 };
 gulp.task('watch', watch);
-gulp.task('default', gulp.parallel('watch', 'browser-sync', 'sass', 'allJs')); // nome das tarefas a serem executadas ao iniciar o gulp
+// tarefas a serem iniciadas ao dar o comando gulp
+gulp.task('default', gulp.parallel('watch', 'browser-sync', 'sass','plugincss', 'allJs', 'pluginjs')); // nome das tarefas a serem executadas ao iniciar o gulp
